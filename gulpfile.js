df67ed7de7f9f59f48adcf405ecb25b06f968e63
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-02-16 13:54:11 +0000

    Move to riffraff.yaml

--- gulpfile.js
+++ gulpfile.js
@@ -54,26 +54,12 @@ gulp.task('riffraff-deploy-dev', ['riffraff-deploy'], function () {
 	gulp.watch(DEPLOY_SOURCE, ['riffraff-deploy']);
 });
 
-
 gulp.task('dev', ['lint-dev', 'cloudformation-dev', 'compile-dev', 'riffraff-deploy-dev']);
 
-gulp.task('archive', ['compile', 'riffraff-deploy'], function () {
-	return gulp.src('tmp/lambda/**/*')
-		.pipe(zip('artifact.zip'))
-		.pipe(gulp.dest('tmp/riffraff/packages/lambda'))
-		.pipe(gulp.dest('tmp/riffraff/packages/sendEmailLambda'));
-});
-
-gulp.task('package', ['archive'], function () {
-	return gulp.src('tmp/riffraff/**/*')
-		.pipe(zip('artifacts.zip'))
-		.pipe(gulp.dest('tmp/'));
-});
-
-gulp.task('deploy', ['package'], function (cb) {
+gulp.task('deploy', ['compile'], function (cb) {
 	riffraff.settings.leadDir = path.join(__dirname, 'tmp/');
 
-	riffraff.s3Upload().then(function () {
+	riffraff.s3FilesUpload().then(function () {
 		cb();
 	}).catch(function (error) {
 		cb(error);
