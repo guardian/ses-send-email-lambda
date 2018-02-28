3cde96009408c4c1d7a69d0bc6b5e3f13dec36e5
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2018-02-28 13:57:09 +0000

    remove redundant gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
-var zip = require('gulp-zip');
 var exec  = require('exec-chainable');
-var yaml = require('gulp-yaml');
 var eslint = require('gulp-eslint');
 var path = require('path');
 process.env.ARTEFACT_PATH = __dirname;
@@ -19,17 +17,6 @@ gulp.task('compile-dev', ['compile'], function () {
 	gulp.watch(LAMBDA_SOURCE, ['compile']);
 });
 
-gulp.task('cloudformation', function () {
-	return gulp.src(CLOUDFORMATION_SOURCE)
-		.pipe(yaml({ space: 4 }))
-		.pipe(gulp.dest('./tmp/riffraff/packages/cloudformation'));
-});
-gulp.task('cfn', ['cloudformation']);
-
-gulp.task('cloudformation-dev', ['cloudformation'], function () {
-	gulp.watch(CLOUDFORMATION_SOURCE, ['cloudformation']);
-});
-
 gulp.task('lint', function () {
 	return gulp.src([
 		LAMBDA_SOURCE,
@@ -43,18 +30,6 @@ gulp.task('lint', function () {
 gulp.task('lint-dev', ['lint'], function () {
 	gulp.watch(LAMBDA_SOURCE, ['lint']);
 });
-
-gulp.task('riffraff-deploy', function () {
-	return gulp.src(DEPLOY_SOURCE)
-		.pipe(yaml({ space: 4 }))
-		.pipe(gulp.dest('tmp/riffraff'));
-});
-
-gulp.task('riffraff-deploy-dev', ['riffraff-deploy'], function () {
-	gulp.watch(DEPLOY_SOURCE, ['riffraff-deploy']);
-});
-
-gulp.task('dev', ['lint-dev', 'cloudformation-dev', 'compile-dev', 'riffraff-deploy-dev']);
 
 gulp.task('deploy', ['compile'], function (cb) {
 	riffraff.settings.leadDir = path.join(__dirname, 'tmp/');
